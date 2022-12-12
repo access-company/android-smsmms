0d8a9f6094ae4176716480584a1a2593ea513525
Takumi Oe <takumi.oe@access-company.com>
2022-12-12 16:06:42 +0900

    Specify mutability of PendingIntent
    See also:
    https://developer.android.com/guide/components/intents-filters?hl=en#DeclareMutabilityPendingIntent

--- library/src/main/java/com/klinker/android/send_message/Transaction.java
+++ library/src/main/java/com/klinker/android/send_message/Transaction.java
@@ -16,6 +16,7 @@
 
 package com.klinker.android.send_message;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.*;
@@ -264,8 +265,13 @@ public class Transaction {
 
                 sentIntent.putExtra("message_uri", messageUri == null ? "" : messageUri.toString());
                 sentIntent.putExtra(SENT_SMS_BUNDLE, sentMessageParcelable);
+                // Workaround for using PendingIntent.FLAG_MUTABLE until compileSdkVersion is updated to 31.
+                // Actual value from:
+                // https://android.googlesource.com/platform/frameworks/base.git/+/android-13.0.0_r18/core/java/android/app/PendingIntent.java#262
+                final int flagMutable = 1<<25;
+                @SuppressLint("WrongConstant")
                 PendingIntent sentPI = PendingIntent.getBroadcast(
-                        context, messageId, sentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                        context, messageId, sentIntent, PendingIntent.FLAG_UPDATE_CURRENT | flagMutable);
 
                 Intent deliveredIntent;
                 if (explicitDeliveredSmsReceiver == null) {
@@ -277,8 +283,9 @@ public class Transaction {
 
                 deliveredIntent.putExtra("message_uri", messageUri == null ? "" : messageUri.toString());
                 deliveredIntent.putExtra(DELIVERED_SMS_BUNDLE, deliveredParcelable);
+                @SuppressLint("WrongConstant")
                 PendingIntent deliveredPI = PendingIntent.getBroadcast(
-                        context, messageId, deliveredIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                        context, messageId, deliveredIntent, PendingIntent.FLAG_UPDATE_CURRENT | flagMutable);
 
                 ArrayList<PendingIntent> sPI = new ArrayList<PendingIntent>();
                 ArrayList<PendingIntent> dPI = new ArrayList<PendingIntent>();
@@ -679,8 +686,13 @@ public class Transaction {
 
             intent.putExtra(MmsSentReceiver.EXTRA_CONTENT_URI, messageUri.toString());
             intent.putExtra(MmsSentReceiver.EXTRA_FILE_PATH, mSendFile.getPath());
+            // Workaround for using PendingIntent.FLAG_MUTABLE until compileSdkVersion is updated to 31.
+            // Actual value from:
+            // https://android.googlesource.com/platform/frameworks/base.git/+/android-13.0.0_r18/core/java/android/app/PendingIntent.java#262
+            final int flagMutable = 1<<25;
+            @SuppressLint("WrongConstant")
             final PendingIntent pendingIntent = PendingIntent.getBroadcast(
-                    context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+                    context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT | flagMutable);
 
             Uri writerUri = (new Uri.Builder())
                     .authority(context.getPackageName() + ".MmsFileProvider")
